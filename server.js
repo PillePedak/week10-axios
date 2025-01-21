81d5d803747c438a904f1871686120c612ec001c
Pille <pille.pedak@tktk.ee>
2025-01-21 15:26:32 +0200

    Added Dialogflow integration and updated dependencies

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const { default: axios } = require('axios');
 const express = require('express');
+const axios = require('axios');
 const http = require('http');
+const dialogflow = require('dialogflow');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,125 +11,161 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Dialogflow konfiguratsioon
+const sessionClient = new dialogflow.SessionsClient({
+  keyFilename: path.join(__dirname, 'config/your-key.json'), // Service account JSON faili tee
+});
 
+const projectId = 'your-project-id'; // Asenda oma Google Cloud projekti ID-ga
+
+// Avaleht
 app.get('/', (req, res) => {
-    let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
-    axios.get(url)
-    .then(response => {
-        console.log(response.data.title);
-        let data = response.data;
-        let releaseDate = new Date(data.release_date).getFullYear();
-        let genres = '';
+  let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
+  axios.get(url).then(response => {
+    console.log(response.data.title);
+    let data = response.data;
+    let releaseDate = new Date(data.release_date).getFullYear();
+    let genres = '';
 
-        data.genres.forEach(genre => {
-            genres = genres + `${genre.name}, `;
-        });
-
-        
-        let genresUpdated = genres.slice(0, -2) + '.';
-        moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
-        console.log(genresUpdated);
-        let currentYear = new Date().getFullYear();
-        res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
+    data.genres.forEach(genre => {
+      genres = genres + `${genre.name}, `;
     });
-    
+
+    let genresUpdated = genres.slice(0, -2) + '.';
+    let moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+    console.log(genresUpdated);
+    let currentYear = new Date().getFullYear();
+    res.render('index', {
+      movieData: data,
+      releaseDate: releaseDate,
+      genres: genresUpdated,
+      poster: moviePoster,
+      year: currentYear
+    });
+  });
 });
 
+// Otsingu leht
 app.get('/search', (req, res) => {
-    res.render('search', { movieDetails:'' });
+  res.render('search', { movieDetails: '' });
 });
 
+// Filmide otsingu POST päring
 app.post('/search', (req, res) => {
-    let userMovieTitle = req.body.movieTitle;
+  let userMovieTitle = req.body.movieTitle;
 
-    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=4ca94f8b470d7e34bd3f59c3914295c8&query=${userMovieTitle}`;
-    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=4ca94f8b470d7e34bd3f59c3914295c8&language=en-US';
-    
-    let endpoints = [movieUrl, genresUrl];
+  let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=4ca94f8b470d7e34bd3f59c3914295c8&query=${userMovieTitle}`;
+  let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=4ca94f8b470d7e34bd3f59c3914295c8&language=en-US';
 
-    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
-    .then(axios.spread((movie, genres) => {
-        const [movieRaw] = movie.data.results;
-        let movieGenreIds = movieRaw.genre_ids;
-        let movieGenres = genres.data.genres;
-        
-        let movieGenresArray = [];
+  let endpoints = [movieUrl, genresUrl];
 
-        for(let i = 0; i < movieGenreIds.length; i++) { // i++ - i = i + 1
-            for(let j = 0; j < movieGenres.length; j++) {
-                if(movieGenreIds[i] === movieGenres[j].id){
-                    movieGenresArray.push(movieGenres[j].name);
-                }
-            }
+  axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+    axios.spread((movie, genres) => {
+      const [movieRaw] = movie.data.results;
+      let movieGenreIds = movieRaw.genre_ids;
+      let movieGenres = genres.data.genres;
+
+      let movieGenresArray = [];
+
+      for (let i = 0; i < movieGenreIds.length; i++) {
+        for (let j = 0; j < movieGenres.length; j++) {
+          if (movieGenreIds[i] === movieGenres[j].id) {
+            movieGenresArray.push(movieGenres[j].name);
+          }
+        }
+      }
+
+      let genresToDisplay = '';
+      movieGenresArray.forEach(genre => {
+        genresToDisplay = genresToDisplay + `${genre}, `;
+      });
+
+      genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+
+      let movieData = {
+        title: movieRaw.title,
+        year: new Date(movieRaw.release_date).getFullYear(),
+        genres: genresToDisplay,
+        overview: movieRaw.overview,
+        posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
+      };
+
+      res.render('search', { movieDetails: movieData });
+    })
+  );
+});
+
+// Dialogflow päring
+app.post('/dialogflow', async (req, res) => {
+  try {
+    const sessionId = req.body.sessionId || 'default-session';
+    const text = req.body.text || 'Hello!';
+
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: text,
+          languageCode: 'en', // Kasuta oma agendi keelekoodi
+        },
+      },
+    };
+
+    const [response] = await sessionClient.detectIntent(request);
+    const result = response.queryResult;
+    res.json({ message: result.fulfillmentText }); // Vastus agentilt
+  } catch (error) {
+    console.error('Dialogflow error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// OMDB API päring
+app.post('/getmovie', (req, res) => {
+  const movieToSearch =
+    req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+      ? req.body.queryResult.parameters.movie
+      : '';
+
+  const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`);
+  http.get(
+    reqUrl,
+    responseFromAPI => {
+      let completeResponse = '';
+      responseFromAPI.on('data', chunk => {
+        completeResponse += chunk;
+      });
+      responseFromAPI.on('end', () => {
+        const movie = JSON.parse(completeResponse);
+        if (!movie || !movie.Title) {
+          return res.json({
+            fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+            source: 'getmovie'
+          });
         }
 
-        let genresToDisplay = '';
-        movieGenresArray.forEach(genre => {
-            genresToDisplay = genresToDisplay+ `${genre}, `;
+        let dataToSend = movieToSearch;
+        dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+          movie.Director
+        } and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+        return res.json({
+          fulfillmentText: dataToSend,
+          source: 'getmovie'
         });
-
-        genresToDisplay = genresToDisplay.slice(0, -2) + '.';
-
-        let movieData = {
-            title: movieRaw.title,
-            year: new Date(movieRaw.release_date).getFullYear(), 
-            genres: genresToDisplay,
-            overview: movieRaw.overview,
-            posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
-        };
-        
-        res.render('search', {movieDetails: movieData});
-    }));
-
+      });
+    },
+    error => {
+      return res.json({
+        fulfillmentText: 'Could not get results at this time',
+        source: 'getmovie'
+      });
+    }
+  );
 });
 
-//new route
-app.post('/getmovie', (req, res) => {
-	const movieToSearch =
-		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
-			? req.body.queryResult.parameters.movie
-			: '';
-
-	const reqUrl = encodeURI(
-		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`
-	);
-	http.get(
-		reqUrl,
-		responseFromAPI => {
-			let completeResponse = ''
-			responseFromAPI.on('data', chunk => {
-				completeResponse += chunk
-			})
-			responseFromAPI.on('end', () => {
-				const movie = JSON.parse(completeResponse);
-                if (!movie || !movie.Title) {
-                    return res.json({
-                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
-                        source: 'getmovie'
-                    });
-                }
-
-				let dataToSend = movieToSearch;
-				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
-					movie.Director
-				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
-
-				return res.json({
-					fulfillmentText: dataToSend,
-					source: 'getmovie'
-				});
-			})
-		},
-		error => {
-			return res.json({
-				fulfillmentText: 'Could not get results at this time',
-				source: 'getmovie'
-			});
-		}
-	)
-});
-
-
+// Serveri käivitamine
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server is running');
+  console.log('server is running');
 });
